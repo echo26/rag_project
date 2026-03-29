09509c3965463eb9cf8e09fdfb055731f1a9befd
eddycho <moonbro.cho@navercorp.com>
2026-03-29 14:05:08 +0900

    feat: make first draft of api server

--- api/app.py
+++ api/app.py
@@ -4,5 +4,10 @@ app = FastAPI()
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
